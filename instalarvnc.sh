165d6962b9d6c08c0738933a78a4eaca81cfc498
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-03-04 22:07:36 +0100

    Update instalarvnc.sh

--- instalarvnc.sh
+++ instalarvnc.sh
@@ -1,4 +1,4 @@
 !/bin/bash
 echo Instala y ejecuta VNC y X11
 apt-get install tightvncserver
-tightvncserver :1 -desktop X -auth /root/.Xauthotity -geometry 1232x800 -depth 16 -frbwait
+tightvncserver :1 -desktop X -auth /root/.Xauthotity -geometry 1232x800 -depth 16
