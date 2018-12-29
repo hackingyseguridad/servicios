75cda9fa31e2b112cf14c907fd4d94ff0d271fd8
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-29 17:49:22 +0100

    Update instalarssh.sh

--- instalarssh.sh
+++ instalarssh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo Script instalar/habilitar sshd y configurar inicio automatico
+# reenvio a puerto 2222: socat -d -d TCP-L:22,reuseaddr,fork SYSTEM:"nc \$SOCAT_PEERADDR 22"
 
 sudo apt-get install ssh
 sudo service ssh start
