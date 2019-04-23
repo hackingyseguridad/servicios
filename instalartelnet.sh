214cc15f56641800e57f4f50e977e1ebdd379999
hackingyseguridad <antonio_taboada@hotmail.es>
2019-04-23 21:55:08 +0200

    Update instalartelnet.sh

--- instalartelnet.sh
+++ instalartelnet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Script instalar/habilitar telnet y configurar inicio automatico
-
+sudo apt-get install openbsd-inetd
 sudo apt-get install telnetd
 sudo /etc/init.d/openbsd-inetd restart
