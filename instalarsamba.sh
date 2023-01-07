b89ec4f407e49dd4b8571e5d9d00fbbe6963dc9f
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-07 12:36:52 +0100

    Update instalarsamba.sh

--- instalarsamba.sh
+++ instalarsamba.sh
@@ -3,4 +3,5 @@ echo "Instala servidor samba y configura arranque automatico al inicio"
 echo "hacking y seguridad .com  2023" 
 apt-get install samba
 systemctl restart smbd
-service samba restart
+service smbd restart
+
