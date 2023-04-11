bc915ba408400ecf9382f888d80b34b917e8ebef
hackingyseguridad <antonio_taboada@hotmail.es>
2023-04-11 16:08:52 +0200

    Create instalarnfs.sh

---
+++ instalarnfs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Instación en KaLi Linux NFS Server, puertos, 111/tcp, 2049/tcp!
+#vim /etc/export
+echo "instalando NFS .."
+
+# Instación 
+sudo apt-get install -y nfs-kernel-server
+sudo systemctl enable nfs-kernel-server
+apt-get install exportfs 
+
+# Proceso automatico
+systemctl start rpcbind nfs-server
+systemctl enable rpcbind nfs-server
+systemctl status nfs-server
+
+# Ver proceso 
+ss -a|grep nfs
