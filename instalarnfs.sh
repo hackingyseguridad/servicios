2377d3dc98ab878c72dd2010426c07a653201e89
hackingyseguridad <antonio_taboada@hotmail.es>
2023-04-13 20:00:20 +0200

    Update instalarnfs.sh

--- instalarnfs.sh
+++ instalarnfs.sh
@@ -40,6 +40,7 @@ nmap -Pn --script=nfs-ls.nse,nfs-showmount.nse,nfs-statfs.nse -p 2049,111  -sVC 
 Desde el lado cliente:
 sudo showmount --exports 192.168.1.200
 # mount -t nfs4 server:/ /mnt/tmp
+sudo mount -t nfs4 192.168.1.200:/tmp /tmp 
 nmap -Pn --script=nfs-ls.nse,nfs-showmount.nse,nfs-statfs.nse -p 2049,111  -sVC -O localhost
 
 
