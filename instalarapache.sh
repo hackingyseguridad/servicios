c55191329afa6261469647b6fa9b923668e57784
hackingyseguridad <antonio_taboada@hotmail.es>
2025-02-08 09:10:30 +0100

    Update instalarapache.sh

--- instalarapache.sh
+++ instalarapache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Instala servidor web apache y configura arranque automatico al inicio
 sudo apt-get install apache2
+sudo apt-get install apache2-utils
 sudo update-rc.d apache2 enable
