9f0a9a7eb712364a3c5adcfc1ef082fe63c38ed6
hackingyseguridad <antonio_taboada@hotmail.es>
2025-02-20 23:54:49 +0100

    Update instalarpostfix.sh

--- instalarpostfix.sh
+++ instalarpostfix.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
-echo "Script instalar postfix"
+# (c) hackingyseguridad.com 2025
 
-apt-get install mailutils
-apt-get install postfix
+cat << "INFO"
+
+▬▬▬.◙.▬▬▬
+═▂▄▄▓▄▄▂
+◢◤ █▀▀████▄▄▄▄◢◤
+█▄ █ █▄ ███▀▀▀▀▀▀▀╬
+◥█████◤
+══╩══╩═
+╬═╬
+╬═╬
+╬═╬
+╬═╬
+╬═╬    subrutina instalar postfix V.1.0 
+╬═╬    hackingyseguridad.com
+╬═╬⛑/
+╬═╬/▌
+╬═╬/  \
+
+INFO
+echo
+echo "."
+echo ".."
+echo "..."
+echo "instalando postfix ... "
+echo "...."
+echo "servidor de correo SMTP "
+echo "....."
+echo
+echo "......"
+echo
+
+apt-get install mailutils -y
+apt-get install postfix -y
 sudo update-rc.d postfix enable
 sudo systemctl enable postfix
 sudo systemctl restart postfix
