991463e88b83dccac4d8110b156aac5928a4b3bc
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-20 20:39:09 +0200

    Update persistencia.sh

--- persistencia.sh
+++ persistencia.sh
@@ -4,9 +4,14 @@
 
 # persisntencia.sh es como hacer que un Script se haga persistente en un sistema
 # hacer que se inicie un Script al inicio del sistema
+# guardar nuestro script en la caroeta /etc/init.d
 # se requiere privilegios SuperUser
 # nuestro script le vamos a llamar implante.sh
 
+
+cd /etc/init.d
+
+# vi implannte.sh   - nuestro script 
 
 sudo chmod +x /etc/init.d/implante.sh
 sudo update-rc.d implante.sh defaults
