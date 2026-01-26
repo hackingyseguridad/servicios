8b09b72f62136370db647731b2f60d0e0be2140e
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-26 01:11:02 +0100

    Create instaladnsmasq.sh

---
+++ instaladnsmasq.sh
@@ -0,0 +1,26 @@
+# Instalar si no está
+apt-get install dnsmasq
+
+# Configurar
+cat > /tmp/dnsmasq.conf << EOF
+# Interface y puerto
+interface=eth0
+listen-address=127.0.0.1
+port=53
+
+# Respuestas A
+address=/google.com/127.0.0.1
+address=/mail.google.com/127.0.0.1
+
+# Registros TXT para email security
+txt-record=google.com,"v=spf1 include:_spf.google.com ~all"
+txt-record=_spf.google.com,"v=spf1 ip4:64.233.160.0/19 ip4:66.102.0.0/20 ip4:66.249.80.0/20 ~all"
+txt-record=_dmarc.google.com,"v=DMARC1; p=none; rua=mailto:dmarc-reports@google.com"
+txt-record=selector1._domainkey.google.com,"v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4Ff+FZy1Fw..."
+
+# Registros MX
+mx-host=google.com,mail.google.com,10
+EOF
+
+# Ejecutar dnsmasq
+sudo dnsmasq -C /tmp/dnsmasq.conf --no-daemon
